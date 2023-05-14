2e4b813d650e73fda5bb4998796d5469edeb5088
Kyllian Aloy <aloykyllian31520@gmail.com>
2023-05-14 18:30:45 +0200

    mise a jour des codes

--- robot.ino
+++ robot.ino
@@ -1,26 +1,169 @@
 /*
   Controlling a servo position using a potentiometer (variable resistor)
-  by Michal Rinott <http://people.interaction-ivrea.it/m.rinott>
+  by Michal Rinott http://people.interaction-ivrea.it/m.rinott
 
   modified on 8 Nov 2013
   by Scott Fitzgerald
   http://www.arduino.cc/en/Tutorial/Knob
+  /
+
+  /
+   genie mais pas drole
+
+   si on eteint avec l'interrupteur du robot la valeur actuelle envoyer en pwm devient la valeur pour laquelle les roues s'arrete
+
 */
 
-/*
- * genie mais pas drole
- *
- * si on eteint avec l'interrupteur du robot la valeur actuelle envoyer en pwm devient la valeur pour laquelle les roues s'arrete
- *
- */
-
 #include <Servo.h>
+
+const int DIST_MAX_D = 15;
+const int DIST_MAX_A = 20;
+
+const int PIN_TRIG_SON_D = 12;
+const int PIN_ECHO_SON_D = 11;
+const int PIN_TRIG_SON_A = 7;
+const int PIN_ECHO_SON_A = 6;
+const int VITESSE_NOMINALE_AVANT = 90 - 15;
+const int VITESSE_NOMINALE_ARRIERE = 90 + 15;
+
+int dist_mur_D = 99;
+int dist_mur_A = 99;
+
+typedef enum Etat
+{
+  INIT,
+  CORECTION_G,
+  CORECTION_D,
+  SCAN,
+  FIN
+};
+
+int vitesse = 0;
+int diff = 0;
 
 Servo rouedroite; // create servo object to control a servo
 Servo rouegauche; // create servo object to control a servo
 
+void Deplacement(int mot_G, int mot_D)
+{
+  rouedroite.write(mot_D);
+  rouegauche.write(mot_G);
+}
+
+void Capt_son()
+{
+  int tmpD;
+  int tmpA;
+
+  digitalWrite(PIN_TRIG_SON_D, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(PIN_TRIG_SON_D, LOW);
+  tmpD = pulseIn(PIN_ECHO_SON_D, HIGH);
+
+  digitalWrite(PIN_TRIG_SON_A, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(PIN_TRIG_SON_A, LOW);
+  tmpA = pulseIn(PIN_ECHO_SON_A, HIGH);
+
+  tmpD = tmpD / 58;
+  tmpA = tmpA / 58;
+
+  if (tmpD >= 0)
+  {
+    dist_mur_D = tmpD;
+  }
+  if (tmpA >= 0)
+  {
+    dist_mur_A = tmpA;
+  }
+}
+
+void MAE_Deplacement()
+{
+  static Etat etat_p = INIT;
+  static Etat etat_s;
+
+  switch (etat_p)
+  {
+  case INIT:
+  {
+    if (dist_mur_D <= DIST_MAX_D)
+    {
+      etat_s = SCAN;
+    }
+    Deplacement(90, 90);
+  }
+  break;
+  case SCAN:
+  {
+    if ((dist_mur_D < DIST_MAX_D) || (dist_mur_A < DIST_MAX_A))
+    { // Si rob se rapproche du mur
+      etat_s = CORECTION_G;
+    }
+    else if (dist_mur_D > DIST_MAX_D)
+    { // Si rob s'eloigne du mur
+      etat_s = CORECTION_D;
+    }
+    Deplacement(VITESSE_NOMINALE_AVANT, VITESSE_NOMINALE_AVANT);
+  }
+  break;
+  case CORECTION_D:
+  {
+    if (dist_mur_D <= DIST_MAX_D)
+    {
+      etat_s = SCAN;
+    }
+    if (dist_mur_A < DIST_MAX_A)
+    {
+      etat_s = CORECTION_G;
+    }
+    Deplacement(VITESSE_NOMINALE_AVANT - 5, VITESSE_NOMINALE_AVANT);
+  }
+  break;
+  case CORECTION_G:
+  {
+    if (dist_mur_D >= DIST_MAX_D && dist_mur_A > DIST_MAX_A)
+    {
+      etat_s = SCAN;
+    }
+    Deplacement(VITESSE_NOMINALE_AVANT, VITESSE_NOMINALE_AVANT - 5);
+  }
+  break;
+  case FIN:
+  {
+    Deplacement(90, 90);
+  }
+  break;
+
+  default:
+  {
+    etat_s = INIT;
+  }
+  break;
+  }
+
+  etat_p = etat_s;
+  Serial.print("Etat = ");
+  Serial.println(etat_p);
+  Serial.print("Distance en cm DROITE: ");
+  Serial.println(dist_mur_D);
+  Serial.print("Distance en cm AVANT: ");
+  Serial.println(dist_mur_A);
+  delay(1000);
+}
+
 void setup()
 {
+  Serial1.begin(9600);
+  pinMode(PIN_TRIG_SON_D, OUTPUT);
+  pinMode(PIN_TRIG_SON_A, OUTPUT);
+
+  digitalWrite(PIN_TRIG_SON_D, LOW);
+  digitalWrite(PIN_TRIG_SON_A, LOW);
+
+  pinMode(PIN_ECHO_SON_D, INPUT);
+  pinMode(PIN_ECHO_SON_A, INPUT);
+
   Serial.begin(9600);
   pinMode(2, INPUT);
   pinMode(3, INPUT);
@@ -32,7 +175,33 @@ void setup()
 
 void loop()
 {
-  Serial.println(digitalRead(2));
-  rouedroite.write(90); // sets the servo position according to the scaled value
-  rouegauche.write(90); // sets the servo position according to the scaled value
+
+  Capt_son();
+  MAE_Deplacement();
+  // lecture bluetooth
+  if (Serial1.available())
+  {
+    int data = Serial1.read();
+    // Serial.println(data);
+  }
+  // 60
+  // 190
+
+  // mode manuel
+  if (data <= 130)
+  {
+    vitesse = data;
+  }
+  else
+  {
+    diff = data - 200; //-10
+  }
+
+  if (vitesse && diff)
+  {
+    rouedroite.write(vitesse + diff); // 60-10=50
+    rouegauche.write(vitesse);        // 60
+  }
+
+  // Deplacement(VITESSE_NOMINALE_AVANT,VITESSE_NOMINALE_AVANT); // Si arret d'urgence
 }
--- telecommande.ino
+++ telecommande.ino
@@ -1,23 +1,32 @@
+/*#include <SPI.h>
+  #include <Wire.h>
+  #include <Adafruit_GFX.h>
+  #include <Adafruit_SSD1306.h>
+  #include <SoftwareSerial.h>
 
+  SoftwareSerial HC05(12, 13);
+*/
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7735.h> // Hardware-specific library for ST7735
+#include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
 #include <SPI.h>
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+#define TFT_CS 10
+#define TFT_RST 8 // Or set to -1 and connect to Arduino RESET pin
+#define TFT_DC 9
+
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
+
 #include <SoftwareSerial.h>
 
-SoftwareSerial HC05(12, 13);
+SoftwareSerial master(4, 5);
+/*
+   CARACTERISTIQUE OLED
+   TAILLE :
+   x : 0 160
+   y : 0 80
+   (a l'envers)
 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
-
-// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-// The pins for I2C are defined by the Wire-library.
-// On an arduino UNO:       A4(SDA), A5(SCL)
-// On an arduino MEGA 2560: 20(SDA), 21(SCL)
-// On an arduino LEONARDO:   2(SDA),  3(SCL), ...
-#define OLED_RESET -1       // Reset pin # (or -1 if sharing Arduino reset pin)
-#define SCREEN_ADDRESS 0x3D ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+*/
 
 // Constants
 #define NUM_JOY 2
@@ -32,16 +41,18 @@ const int joyOffset = 0;
 // Variables
 int joyVal[NUM_JOY] = {0, 0};
 
+int pixel[200][2];
+int num_pixel = 0;
+
 void setup()
 {
     // put your setup code here, to run once:
     Serial.begin(9600);
-
-    HC05.begin(38400); // initialisation du module bluetooth
-
-    display.clearDisplay();                   // clear
-    display.drawPixel(10, 10, SSD1306_WHITE); // dessiner un point blanc
-    display.display();                        // write
+    master.begin(9600);
+    tft.initR(INITR_BLACKTAB);   // Init ST7735S chip, black tab
+    tft.initR(INITR_MINI160x80); // Init ST7735S mini display
+    tft.fillScreen(ST77XX_BLACK);
+    tft.invertDisplay(true);
 
     for (int i = 0; i < NUM_JOY; i++)
         pinMode(joyPin[i], INPUT);
@@ -50,37 +61,72 @@ void setup()
 
 void loop()
 {
-    String message;
-    while (HC05.available())
+    /*
+      String message;
+      while (HC05.available())
+      {
+      delay(3);
+      char c = HC05.read();
+      message += c;
+      }
+
+      //test bluetooth
+
+      if (message.length() > 0) {
+      num_pixel++;
+      int x = 0, y = 0;
+      int pos = message.indexOf('/');
+      String subx = message.substring(0, pos);
+      String suby = message.substring(pos + 1, message.length());
+      x = subx.toInt();
+      y = suby.toInt();
+      tft.drawPixel(x, y, ST77XX_GREEN);
+      pixel[num_pixel][0]=x;
+      pixel[num_pixel][1]=y;
+      //Serial.println(message);
+      }*/
+    if (num_pixel == 199)
     {
-        delay(3);
-        char c = HC05.read();
-        message += c;
-    }
-
-    // test bluetooth
-
-    if (message.length() > 0)
-    {
-        int x = 0, y = 0;
-        int pos = message.indexOf('/');
-        String subx = message.substring(0, pos);
-        String suby = message.substring(pos + 1, message.length());
-        x = subx.toInt();
-        y = suby.toInt();
-
-        display.drawPixel(x, y, SSD1306_WHITE); // dessiner un point blanc
-        display.display();                      // write
-        // Serial.println(message);
+        num_pixel = 0;
+        for (int i = 0; i < 199; i++)
+        {
+            pixel[num_pixel][0] = 0;
+            pixel[num_pixel][1] = 0;
+        }
     }
 
     readJoystick();
+
+    joyVal[0] = map(joyVal[0], 0, 1023, 130, 50);
+    joyVal[1] = map(joyVal[1], 0, 1023, 190, 210);
+    //  Serial.print(joyVal[0]);
+    //  Serial.print("\t");
+    //  Serial.println(joyVal[1]);
+    master.write(joyVal[0]);
+    master.write(joyVal[1]);
+
+    /*
+      byte buffer[4];
+      buffer[0] = (value >> 24) & 0xFF; // Extraire le premier octet
+      buffer[1] = (value >> 16) & 0xFF; // Extraire le deuxième octet
+      buffer[2] = (value >> 8) & 0xFF; // Extraire le troisième octet
+      buffer[3] = value & 0xFF; // Extraire le quatrième octet
+      for (int i = 0; i < 4; i++) {
+        Serial.println(buffer[i]);
+        master.write(buffer[i]); // Envoyer chaque octet individuellement
+      }
+    */
+
+    // master.write((uint8_t*)&value1, sizeof(value1));
+    // envoyer valeur Joystick  bluetooth
+
+    tft.drawPixel(40, 80, ST77XX_GREEN);
 }
 
-float joyRawToPhys(int raw)
+int joyRawToPhys(int raw)
 { /* function joyRawToPhys */
     ////Joystick conversion rule
-    float phys = map(raw, MIN_VAL, MAX_VAL, -100 + joyOffset, 100 + joyOffset) - joyOffset;
+    int phys = map(raw, MIN_VAL, MAX_VAL, 0 + joyOffset, 20 + joyOffset) - joyOffset;
     return phys;
 }
 
@@ -90,10 +136,6 @@ void readJoystick()
     for (int i = 0; i < NUM_JOY; i++)
     {
         joyVal[i] = analogRead(joyPin[i]);
-        Serial.print(F("joy"));
-        Serial.print(i);
-        Serial.print(F(" : "));
-        Serial.println(joyVal[i]);
     }
     for (int i = 0; i < NUM_JOY; i++)
     {
@@ -104,5 +146,87 @@ void readJoystick()
     if (!digitalRead(joyBtn))
     {
         Serial.println(F("Joy Button pressed"));
+        centre();
+        delay(500);
+    }
+    num_pixel++;
+    pixel[num_pixel][0] = joyRawToPhys(joyVal[0]);
+    pixel[num_pixel][1] = joyRawToPhys(joyVal[1]);
+    tft.drawPixel(pixel[num_pixel][0], pixel[num_pixel][1], ST77XX_GREEN);
+}
+
+void dezoom()
+{
+    tft.fillScreen(ST77XX_BLACK);
+    for (int nb = 0; nb < num_pixel; nb++)
+    {
+        pixel[nb][0] = ((pixel[nb][0] - 40) / 1.3) + 40;
+        pixel[nb][1] = ((pixel[nb][1] - 80) / 1.6) + 80;
+        tft.drawPixel(pixel[nb][0], pixel[nb][1], ST77XX_GREEN);
+    }
+}
+
+void zoom()
+{
+    tft.fillScreen(ST77XX_BLACK);
+    for (int nb = 0; nb < num_pixel; nb++)
+    {
+        pixel[nb][0] = ((pixel[nb][0] - 40) * 1.3) + 40;
+        pixel[nb][1] = ((pixel[nb][1] - 80) * 1.6) + 80;
+        if (pixel[nb][1] > 180)
+            pixel[nb][1] = 180;
+        if (pixel[nb][0] > 80)
+            pixel[nb][0] = 80;
+        tft.drawPixel(pixel[nb][0], pixel[nb][1], ST77XX_GREEN);
+    }
+}
+
+void centre()
+{
+    tft.fillScreen(ST77XX_BLACK);
+    int maxx = pixel[0][0];
+    int maxy = pixel[0][1];
+    int minx = pixel[0][0];
+    int miny = pixel[0][1];
+    for (int nb = 0; nb < num_pixel; nb++)
+    {
+        if (maxx < pixel[nb][0])
+        {
+            maxx = pixel[nb][0];
+        }
+        if (maxy < pixel[nb][1])
+        {
+            maxy = pixel[nb][1];
+        }
+        if (minx > pixel[nb][0])
+        {
+            minx = pixel[nb][0];
+        }
+        if (miny > pixel[nb][1])
+        {
+            miny = pixel[nb][1];
+        }
+    }
+    int diffx = (maxx - minx) / 2; // 40 = (80-0)/2
+    int diffy = (maxy - miny) / 2; // 20 = (40-0)/2
+
+    diffx = (tft.width() / 2) - diffx;
+    diffy = (tft.height() / 2) - diffy;
+    Serial.print(maxx);
+    Serial.print("\t");
+    Serial.print(minx);
+    Serial.print("\t");
+    Serial.print(maxy);
+    Serial.print("\t");
+    Serial.print(miny);
+    Serial.print("\t");
+    Serial.println(num_pixel);
+    for (int nb = 0; nb < num_pixel; nb++)
+    {
+        pixel[nb][0] = pixel[nb][0] + diffx;
+        pixel[nb][1] = pixel[nb][1] + diffy;
+        // pixel[nb][0] =  map(pixel[nb][0], minx, maxx, tft.height() / 2 - diffx , tft.height() / 2 + diffx);
+        // pixel[nb][1] =  map(pixel[nb][1], miny, maxy, tft.width() / 2 - diffy, tft.width() / 2 + diffy);
+        tft.drawPixel(pixel[nb][0], pixel[nb][1], ST77XX_GREEN);
     }
 }
